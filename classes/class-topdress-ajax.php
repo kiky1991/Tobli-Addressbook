631bddda2608dabe668e329b8284c2f0b6fa0409
Hengky <kiky1991@gmail.com>
2020-12-09 23:23:47 +0700

    resolve button size

--- classes/class-topdress-ajax.php
+++ classes/class-topdress-ajax.php
@@ -85,7 +85,7 @@ class TOPDRESS_Ajax
         if ($addresses) {
             $data = array();
             foreach ($addresses as $address) {
-                $set_default = ($address_id !== $address['id_address']) ? ('<a class="btn small black" id="set-address-book" title="Set as Default" address-id="' . $address['id_address'] . '"><img style="width:24px;height:24px" src="' . TOPDRESS_PLUGIN_URI . '/assets/img/paper-push-pin.png' . '"></a>') : '';
+                $set_default = ($address_id !== $address['id_address']) ? ('<a class="btn small black" id="set-address-book" title="Set as Default" address-id="' . $address['id_address'] . '"><img src="' . TOPDRESS_PLUGIN_URI . '/assets/img/paper-push-pin.png' . '"></a>') : '';
                 $data[] = array(
                     $address['id_address'],
                     wp_sprintf('%1$s %2$s', __($address['first_name']), __($address['last_name'])),
@@ -93,8 +93,8 @@ class TOPDRESS_Ajax
                     $address['district'],
                     $address['city'],
                     $address['tag'],
-                    '<a class="btn small black" title="Edit" href="' . wc_get_endpoint_url('edit-address/edit-addressbook?id=' . $address['id_address'], '', wc_get_page_permalink('myaccount')) . '"><img style="width:24px;height:24px" src="' . TOPDRESS_PLUGIN_URI . '/assets/img/edit.png' . '"></a>' . '&nbsp;' .
-                        '<a class="btn small black" title="Delete" id="delete-address-book" address-id="' . $address['id_address'] . '"><img style="width:24px;height:24px" src="' . TOPDRESS_PLUGIN_URI . '/assets/img/trash.png' . '"></a>&nbsp;' .
+                    '<a class="btn small black" title="Edit" href="' . wc_get_endpoint_url('edit-address/edit-addressbook?id=' . $address['id_address'], '', wc_get_page_permalink('myaccount')) . '"><img src="' . TOPDRESS_PLUGIN_URI . '/assets/img/edit.png' . '"></a>' . '&nbsp;' .
+                        '<a class="btn small black" title="Delete" id="delete-address-book" address-id="' . $address['id_address'] . '"><img src="' . TOPDRESS_PLUGIN_URI . '/assets/img/trash.png' . '"></a>&nbsp;' .
                         $set_default
                 );
             }
